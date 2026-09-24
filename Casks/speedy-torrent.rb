f52007262df073f194bacf599fa0cb21bb80a008
Nick Fay <nwfay@icloud.com>
2026-09-24 18:30:32 +0100

    speedy-torrent 0.4

--- Casks/speedy-torrent.rb
+++ Casks/speedy-torrent.rb
@@ -1,6 +1,6 @@
 cask "speedy-torrent" do
-  version "0.3"
-  sha256 "ce817ba0680409197893453d0c86050a943b59757414abda1b81415819ea6d50"
+  version "0.4"
+  sha256 "095b4b513a92c6f4517e84f6c02ea9e357a048ef3be083e5c8a3923e30fb1df1"
 
   url "https://github.com/n14395/speedy-torrent/releases/download/v#{version}/SpeedyTorrent-#{version}.dmg"
   name "SpeedyTorrent"
@@ -12,6 +12,7 @@ cask "speedy-torrent" do
     strategy :github_latest
   end
 
+  auto_updates true
   depends_on arch: :arm64
   depends_on macos: :sequoia
 
