90aae7d0a5f48d812aeaa962fded78aa93d10f2f
Nick Fay <nwfay@icloud.com>
2026-09-24 20:21:37 +0100

    icon-setter 0.3

--- Casks/icon-setter.rb
+++ Casks/icon-setter.rb
@@ -1,6 +1,6 @@
 cask "icon-setter" do
-  version "0.2"
-  sha256 "40366ec88d3a7160817ef24fc00b5b38ccd78c9637560e8388f673932ff78d2d"
+  version "0.3"
+  sha256 "61845c1e169312ebbaeba663f748ae3cd78182925796e63709c7215d9965ccca"
 
   url "https://github.com/n14395/icon-setter/releases/download/v#{version}/IconSetter-#{version}.dmg"
   name "Icon Setter"
@@ -18,14 +18,14 @@ cask "icon-setter" do
 
   app "IconSetter.app"
 
-  uninstall quit: "com.n14395.icon-setter"
+  uninstall quit: "com.n14395.IconSetter"
 
   zap trash: [
-    "~/Library/Application Support/com.n14395.icon-setter",
+    "~/Library/Application Support/com.n14395.IconSetter",
     "~/Library/Application Support/IconSetter",
-    "~/Library/Caches/com.n14395.icon-setter",
-    "~/Library/HTTPStorages/com.n14395.icon-setter",
-    "~/Library/Preferences/com.n14395.icon-setter.plist",
-    "~/Library/Saved Application State/com.n14395.icon-setter.savedState",
+    "~/Library/Caches/com.n14395.IconSetter",
+    "~/Library/HTTPStorages/com.n14395.IconSetter",
+    "~/Library/Preferences/com.n14395.IconSetter.plist",
+    "~/Library/Saved Application State/com.n14395.IconSetter.savedState",
   ]
 end
